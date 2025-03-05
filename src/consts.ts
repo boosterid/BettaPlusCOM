4cf2a1121f2305ceabcb25cab45c276d7b3b4617
Agung <abu.naufal0312@gmail.com>
2025-03-05 23:32:15 +0700

    inisialisasi ulang repo

---
+++ src/consts.ts
@@ -0,0 +1,40 @@
+import type { Metadata, Site, Socials } from "@types";
+
+export const SITE: Site = {
+  TITLE: "BettaPlus.com",
+  DESCRIPTION: "Temukan informasi lengkap tentang ikan cupang (Betta splendens), mulai dari perawatan, budidaya, jenis terbaik, hingga tren terbaru. Dapatkan tips dan panduan dari ahli untuk memastikan ikan cupang Anda tetap sehat dan menawan. Kunjungi BettaPlus.com sekarang!",
+  EMAIL: "abu.naufal0312@gmail.com",
+  NUM_POSTS_ON_HOMEPAGE: 5,
+  NUM_PROJECTS_ON_HOMEPAGE: 3,
+};
+
+export const HOME: Metadata = {
+  TITLE: "Home",
+  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+};
+
+export const BLOG: Metadata = {
+  TITLE: "Blog",
+  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+};
+
+export const PROJECTS: Metadata = {
+  TITLE: "Achievements",
+  DESCRIPTION:
+    "A collection of my projects with links to repositories and live demos.",
+};
+
+export const SOCIALS: Socials = [
+  {
+    NAME: "X (formerly Twitter)",
+    HREF: "",
+  },
+  {
+    NAME: "GitHub",
+    HREF: "",
+  },
+  {
+    NAME: "Website",
+    HREF: "",
+  },
+];
